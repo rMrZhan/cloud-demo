68c1b911b8512610ad859cc2d1a90e927376d519
zhl <895902052@qq.com>
2019-05-17 14:14:06 +0800

    注册Eureka

--- consumer-demo/src/main/java/cn/it/cast/ConsumerDemoApplication.java
+++ consumer-demo/src/main/java/cn/it/cast/ConsumerDemoApplication.java
@@ -2,10 +2,11 @@ package cn.it.cast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerDemoApplication {
 
--- consumer-demo/src/main/java/cn/it/cast/consumer/pojo/controller/ConsumerController.java
+++ consumer-demo/src/main/java/cn/it/cast/consumer/pojo/controller/ConsumerController.java
@@ -3,23 +3,31 @@ package cn.it.cast.consumer.pojo.controller;
 import cn.it.cast.consumer.pojo.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("consume")
+@RequestMapping("consumer")
 @Slf4j
 public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
-
+    @Autowired
+    private DiscoveryClient  discoveryClient;
     @GetMapping("{id}")
-    public User queryById(@PathVariable("id") Long id){
+    public User queryById(@PathVariable("id") Long id) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        ServiceInstance instance = instances.get(0);
         log.info("hello world");
-        String url = "http://localhost:8081/user/" + id;
+        String url = "http://"+ instance.getHost() + ":" + instance.getPort() +"/user/" + id;
+        log.info("url ======"+url);
         return restTemplate.getForObject(url, User.class);
     }
 }
